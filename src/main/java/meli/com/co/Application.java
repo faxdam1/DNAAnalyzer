f2ff244b2b7dcbd306aa326a8c39e2e73fad92f0
David Arango <daviarmo>
2022-03-29 18:01:59 -0500

    add DnaAnalyzer

--- src/main/java/meli/com/co/Application.java
+++ src/main/java/meli/com/co/Application.java
@@ -2,101 +2,11 @@ package meli.com.co;
 
 import io.micronaut.runtime.Micronaut;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Application {
 
-
     public static void main(String[] args) {
-
-        Application a = new Application();
-        //System.out.println("mutante: "+ a.searchMutantADN(new String []{"TTGTGA","CAGTGC","TTATTT","AGACGG","GCGTCA","TCACTG"}));
-        //System.out.println("mutante: "+a.searchMutantADN(new String []{"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","ATGCGG"}));
-        System.out.println("mutante: "+a.searchMutantADN(new String []{"AAAAAA","AAGTGC","ATCTTT","AAAAGG","GCGTCA","TCCCTG"}));
-        // Micronaut.run(Application.class);
+        Micronaut.run(Application.class);
     }
-
-    private HashMap<String, Integer> hashTable = new HashMap<>();
-    private String [] dictionary=new String[]{"AAAA","TTTT","CCCC","GGGG"};
-
-    private boolean searchMutantADN(String[] dnaSecuence) {
-        String[][] dna = this.convertArray(dnaSecuence);
-        for (int row = 0; row < dna.length; row++) {
-            for (int col = 0; col < dna[row].length; col++) {
-                this.indexSecuenceNode(dna, row, col);
-            }
-        }
-        int countSecuence=0;
-        for(String secuence:dictionary){
-            if(this.hashTable.containsKey(secuence))
-                countSecuence = countSecuence + this.hashTable.get(secuence).intValue();
-        }
-        return countSecuence>1;
-    }
-
-    private void indexSecuenceNode(String[][] dna, int row, int col) {
-        int[] x = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int[] y = {-1, 0, 1, -1, 1, -1, 0, 1};
-        int R = dna.length, C = dna[0].length;
-        int len = 4;
-        for (int dir = 0; dir < 8; dir++) {
-            int rd = row + x[dir];
-            int cd = col + y[dir];
-            String sequence = this.findSequence(R, C, len, rd, cd, dna, row, col, x, y, dir);
-            if (sequence.length() == len) {
-                this.markSequence(R, C, len, rd, cd, dna, row, col, x, y, dir);
-                this.indexSequence(sequence);
-            }
-        }
-    }
-
-    private String findSequence(int R, int C, int len, int rd, int cd, String dna[][], int row, int col, int[] x, int y[], int dir) {
-        String sequence = "" + dna[row][col];
-        for (int k = 1; k < len; k++) {
-            if (rd >= R || rd < 0 || cd >= C || cd < 0) {
-                break;
-            }
-            if (dna[row][col].equals(dna[rd][cd]) && !dna[rd][cd].equals("X")) {
-                sequence = sequence + dna[rd][cd];
-            } else {
-                break;
-            }
-            rd += x[dir];
-            cd += y[dir];
-        }
-        return sequence;
-    }
-
-    private void indexSequence(String word) {
-        if (this.hashTable.containsKey(word)) {
-            this.hashTable.put(word, this.hashTable.get(word).intValue() + 1);
-        } else {
-            this.hashTable.put(word, Integer.valueOf(1));
-        }
-    }
-
-    private void markSequence(int R, int C, int len, int rd, int cd, String dna[][], int row, int col, int[] x, int y[], int dir) {
-        dna[row][col] = "X";
-        for (int k = 1; k < len; k++) {
-            if (rd >= R || rd < 0 || cd >= C || cd < 0) {
-                break;
-            }
-            dna[rd][cd] = "X";
-            rd += x[dir];
-            cd += y[dir];
-        }
-    }
-
-    private String[][] convertArray(String[] array) {
-        String[][] dna = new String[array.length][array[0].length()];
-        for (int i = 0; i < array.length; i++) {
-            dna[i] = array[i].split("");
-        }
-        return dna;
-    }
-
 
 }
--- src/main/java/meli/com/co/domain/service/DnaAnalyzerService.java
+++ src/main/java/meli/com/co/domain/service/DnaAnalyzerService.java
@@ -3,68 +3,89 @@ package meli.com.co.domain.service;
 import meli.com.co.domain.exception.ExceptionFactory;
 import reactor.core.publisher.Mono;
 
+import java.util.HashMap;
+
 
 public class DnaAnalyzerService extends Service {
 
+    private HashMap<String, Integer> hashTable = new HashMap<>();
+    private String [] dictionary=new String[]{"AAAA","TTTT","CCCC","GGGG"};
 
-
-
-
-
-
-    //public Mono<Boolean> isMutant(String[] dna){
-
-
-        //return Mono.just()
-    //}
-
-
-
-   /* public  Mono<Void>  rowValidate(String ) {
-        return Mono.just(value).filter(it-> ("ATCG".contains(value)))
-                .switchIfEmpty(Mono.error(ExceptionFactory.INVALID_DNA.get()))
-                .then(Mono.empty());
-    }*/
-
-    public  Mono<Void>  characterValidate(String value) {
-        return Mono.just(value).filter(it-> ("ATCG".contains(value)))
-                .switchIfEmpty(Mono.error(ExceptionFactory.INVALID_DNA.get()))
-                .then(Mono.empty());
+    public boolean isMutant(String[] dnaSecuence) {
+        String[][] dna = this.convertArray(dnaSecuence);
+        for (int row = 0; row < dna.length; row++) {
+            for (int col = 0; col < dna[row].length; col++) {
+                this.indexSecuenceNode(dna, row, col);
+            }
+        }
+        int countSecuence=0;
+        for(String secuence:dictionary){
+            if(this.hashTable.containsKey(secuence))
+                countSecuence = countSecuence + this.hashTable.get(secuence).intValue();
+        }
+        return countSecuence>1;
     }
 
-
-    /*
-    private ClienteRepositoryI clienteRepository;
-
-    public ClienteService(ClienteRepositoryI clienteRepository){
-        this.clienteRepository=clienteRepository;
+    private void indexSecuenceNode(String[][] dna, int row, int col) {
+        int[] x = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] y = {-1, 0, 1, -1, 1, -1, 0, 1};
+        int R = dna.length, C = dna[0].length;
+        int len = 4;
+        for (int dir = 0; dir < 8; dir++) {
+            int rd = row + x[dir];
+            int cd = col + y[dir];
+            String sequence = this.findSequence(R, C, len, rd, cd, dna, row, col, x, y, dir);
+            if (sequence.length() == len) {
+                this.markSequence(R, C, len, rd, cd, dna, row, col, x, y, dir);
+                this.indexSequence(sequence);
+            }
+        }
     }
 
-    public Mono<Boolean> guardarCliente(final Cliente cliente) {
-        return Validate.nullEntityValidate(cliente, "Cliente")
-                .switchIfEmpty(cliente.validate())
-                .then(clienteRepository.guardarCliente(cliente))
-                .onErrorResume(error-> {
-                     log.error(error.getMessage());
-                     return Mono.error(error);
-                 });
+    private String findSequence(int R, int C, int len, int rd, int cd, String dna[][], int row, int col, int[] x, int y[], int dir) {
+        String sequence = "" + dna[row][col];
+        for (int k = 1; k < len; k++) {
+            if (rd >= R || rd < 0 || cd >= C || cd < 0) {
+                break;
+            }
+            if (dna[row][col].equals(dna[rd][cd]) && !dna[rd][cd].equals("X")) {
+                sequence = sequence + dna[rd][cd];
+            } else {
+                break;
+            }
+            rd += x[dir];
+            cd += y[dir];
+        }
+        return sequence;
     }
 
-    public Mono<Cliente> consultarCliente(final String identidad) {
-        return Validate.nullOrEmptyValidate(identidad, "identidad")
-                .then(clienteRepository.consultarCliente(identidad))
-                .switchIfEmpty(Mono.error(ExceptionFactory.DNA_NO_MUTANT.get()))
-                .onErrorResume(error-> {
-                      log.error(error.getMessage());
-                      return Mono.error(error);
-                });
+    private void indexSequence(String secuence) {
+        if (this.hashTable.containsKey(secuence)) {
+            this.hashTable.put(secuence, this.hashTable.get(secuence).intValue() + 1);
+        } else {
+            this.hashTable.put(secuence, Integer.valueOf(1));
+        }
     }
 
-    public Flux<Cliente> consultarClientes() {
-        return clienteRepository.consultarClientes();
+    private void markSequence(int R, int C, int len, int rd, int cd, String dna[][], int row, int col, int[] x, int y[], int dir) {
+        dna[row][col] = "X";
+        for (int k = 1; k < len; k++) {
+            if (rd >= R || rd < 0 || cd >= C || cd < 0) {
+                break;
+            }
+            dna[rd][cd] = "X";
+            rd += x[dir];
+            cd += y[dir];
+        }
     }
-    */
 
+    private String[][] convertArray(String[] array) {
+        String[][] dna = new String[array.length][array[0].length()];
+        for (int i = 0; i < array.length; i++) {
+            dna[i] = array[i].split("");
+        }
+        return dna;
+    }
 
 }
 
